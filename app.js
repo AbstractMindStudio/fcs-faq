a6758b70ff53fb8dd41560be7692bf71e8b39fef
Ivan-Istomin <ivan@istomin.im>
2015-09-11 01:13:52 +0300

    [+] First commit

---
+++ app.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var hbs = require('express-hbs');
+var routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+// Use `.hbs` for extensions and find partials in `views/partials`.
+app.engine('hbs', hbs.express4({
+    partialsDir: __dirname + '/views/partials'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+//if (app.get('env') === 'development') {
+//    app.use(function(err, req, res, next) {
+//        res.status(err.status || 500);
+//        res.render('error', {
+//            message: err.message,
+//            error: err
+//        });
+//    });
+//}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+var GoogleSpreadsheet = require("google-spreadsheet");
+
+var my_sheet = new GoogleSpreadsheet('172SQyiLXHDOmQjtWxhu8uRmJPmcDYEd0wcVa1f7A5P0');
+
+//my_sheet.getRows(1, function(err, row_data){
+//    console.log( 'pulled in '+row_data.length + ' rows');
+//});
+
+
+
+module.exports = app;

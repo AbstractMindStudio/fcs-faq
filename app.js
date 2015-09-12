ca9ec95ff6b0969f44096decba5484eb947c1344
Ivan-Istomin <ivan@istomin.im>
2015-09-12 09:57:08 +0300

    [+] Parse from Google Table

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -50,22 +50,12 @@ app.use(function(req, res, next) {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
         error: {}
     });
 });
-
-var GoogleSpreadsheet = require("google-spreadsheet");
-
-var my_sheet = new GoogleSpreadsheet('172SQyiLXHDOmQjtWxhu8uRmJPmcDYEd0wcVa1f7A5P0');
-
-//my_sheet.getRows(1, function(err, row_data){
-//    console.log( 'pulled in '+row_data.length + ' rows');
-//});
-
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,45 @@ var express = require('express');
 var GoogleSpreadsheet = require("google-spreadsheet");
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var mySheet = new GoogleSpreadsheet('172SQyiLXHDOmQjtWxhu8uRmJPmcDYEd0wcVa1f7A5P0');
+
+
+  mySheet.getRows(1, function(err, rowData) {
+
+    var itemArr = [];
+    var cat = [];
+    var catN = [];
+
+    rowData.forEach(function(item) {
+        if(cat.indexOf(item['h']) == -1) {
+            cat.push(item['h']);
+        }
+    });
+
+      var i = 1;
+      cat.forEach(function(item) {
+          catN.push({name: item, cls: 'cat' + i++});
+      });
+
+      var j = 0
+
+      rowData.forEach(function(item) {
+          catN.forEach(function(elem) {
+              if(elem.name == item['h']) {
+                  itemArr[elem.cls] = {name: elem.name, qat: []};
+                  itemArr[elem.cls].qat.push({a: item['a'], q: item['q'], t:item['t'],
+                      cls: elem.cls, c: item['h']});
+              }
+          });
+      });
+
+      console.log(itemArr);
+
+    res.render('index', { title: 'FAQ ФКН',  itemArray: itemArr, cat: catN });
+  });
+
+
 });
 
 module.exports = router;
